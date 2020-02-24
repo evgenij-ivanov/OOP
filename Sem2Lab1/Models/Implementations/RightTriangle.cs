68867300c27a82b18912acf1c582ab84a89e2532
Evgeniy Ivanov <timeescapenow@gmail.com>
2020-02-24 17:01:36 +0300

    Add perimeter for Right Triangle

--- Sem2Lab1/Models/Implementations/RightTriangle.cs
+++ Sem2Lab1/Models/Implementations/RightTriangle.cs
@@ -19,7 +19,6 @@ namespace Sem2Lab1.Implementations
         {
             get
             {
-                double angleC = 180.0 - AngleA - AngleB;
                 if (Geometry.AreAnglesEqual(AngleA, 90.0))
                 {
                     double angleB = Geometry.DegreesToRadians(AngleB);
@@ -42,6 +41,32 @@ namespace Sem2Lab1.Implementations
             }
         }
 
-        public double Perimeter => throw new NotImplementedException();
+        public double Perimeter
+        {
+            get
+            {
+                if (Geometry.AreAnglesEqual(AngleA, 90.0))
+                {
+                    double angleB = Geometry.DegreesToRadians(AngleB);
+                    double height = Math.Tan(angleB) * EdgeLength / 2.0;
+                    double hypotenuse = Math.Sqrt(EdgeLength * EdgeLength + height * height);
+                    return hypotenuse + EdgeLength + height;
+                }
+                else if (Geometry.AreAnglesEqual(AngleB, 90.0))
+                {
+                    double angleA = Geometry.DegreesToRadians(AngleA);
+                    double height = Math.Tan(angleA) * EdgeLength / 2.0;
+                    double hypotenuse = Math.Sqrt(EdgeLength * EdgeLength + height * height);
+                    return hypotenuse + EdgeLength + height;
+                }
+                else
+                {
+                    double angleA = Geometry.DegreesToRadians(AngleA);
+                    double edgeA = Math.Sin(angleA) * EdgeLength;
+                    double edgeB = Math.Sqrt(EdgeLength * EdgeLength - edgeA * edgeA);
+                    return edgeA + edgeB + EdgeLength;
+                }
+            }
+        }
     }
 }
